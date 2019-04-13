a78f61a24ed0b3f9aa74f74deed65c7b4a322bd7
icecubewang <wangfeilan@hotmail.com>
2019-04-13 08:34:48 -0700

    Add partial logic for camera.

--- project1/ThreeFunctionsViewController.swift
+++ project1/ThreeFunctionsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ThreeFunctionsViewController: UIViewController {
+class ThreeFunctionsViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,7 +17,23 @@ class ThreeFunctionsViewController: UIViewController {
     }
     
     @IBAction func CameraButton(_ sender: Any) {
+        let vc = UIImagePickerController()
+        vc.sourceType = .camera
+        vc.allowsEditing = true
+        vc.delegate = self
+        present(vc, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
         
+        guard let image = info[.editedImage] as? UIImage else {
+            print("No image found")
+            return
+        }
+        
+        // print out the image size as a test
+        print(image.size)
     }
     
     /*

9cba62a959e33cdf17905a4db4d2ed370baa5a2e
Yujie <fangyujie@g.ucla.edu>
2019-04-12 23:57:58 -0700

    Add camera

---
+++ project1/CameraViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CameraViewController.swift
+//  project1
+//
+//  Created by Yujie Fang on 4/12/19.
+//  Copyright © 2019 Rosalie Ma. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIImagePickerController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ project1/ThreeFunctionPageViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ThreeFunctionPageViewController.swift
+//  project1
+//
+//  Created by Yujie Fang on 4/12/19.
+//  Copyright © 2019 Rosalie Ma. All rights reserved.
+//
+
+import UIKit
+
+class ThreeFunctionPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func CameraButton(_ sender: Any) {
+//        let vc = UIImagePickerController()
+//        vc.sourceType = .camera
+//        vc.allowsEditing = true
+//        vc.delegate = self
+//        present(vc, animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

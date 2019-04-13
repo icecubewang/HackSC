8b7e2347a2b66e2d6f5c9d89b41641b4b4c34e25
John Yuan <yjohnyuan@gmail.com>
2019-04-12 23:31:08 -0700

    Added Main Firebase Configuration
    Added Main Firebase Configuration

--- project1/AppDelegate.swift
+++ project1/AppDelegate.swift
@@ -8,14 +8,17 @@
 
 import UIKit
 
+import Firebase
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // Use Firebase library to configure APIs
+        FirebaseApp.configure()
         return true
     }
 

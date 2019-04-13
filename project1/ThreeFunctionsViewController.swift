9daebe0529e40db11eeb63139f8073f6b1bac496
icecubewang <wangfeilan@hotmail.com>
2019-04-13 08:20:12 -0700

    Add VC for 3 functions and button for camera.

---
+++ project1/ThreeFunctionsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ThreeFunctionsViewController.swift
+//  project1
+//
+//  Created by Feilan Wang on 13/4/19.
+//  Copyright © 2019 Rosalie Ma. All rights reserved.
+//
+
+import UIKit
+
+class ThreeFunctionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func CameraButton(_ sender: Any) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

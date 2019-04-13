f971fca5f9c30528d8b5ea584ef1972fafa85770
Xiwei Ma <xiwei@ucla.edu>
2019-04-12 21:50:34 -0700

    add login

--- project1/ViewController.swift
+++ project1/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func User(_ sender: Any) {
+    }
+    
+    @IBAction func Password(_ sender: Any) {
+    }
+    
+    @IBAction func signin(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
 

730c9e0dd6a90014c0b7d23237adab36e13dafd2
icecubewang <wangfeilan@hotmail.com>
2019-04-13 14:52:54 -0700

    add create account function

--- project1/ViewController.swift
+++ project1/ViewController.swift
@@ -7,22 +7,41 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
 class ViewController: UIViewController {
 
-    @IBAction func User(_ sender: Any) {
-    }
+    var ref: DatabaseReference?
     
-    @IBAction func Password(_ sender: Any) {
-    }
-    
-    @IBAction func signin(_ sender: UIButton) {
-    }
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        self.ref = Database.database().reference()
+    }
+    
+    @IBAction func createAccount(_ sender: UIButton) {
+        let user_name = username.text!
+        let pass_word = password.text!
+        
+        self.ref?.child("UserAccountInfo").child(user_name).setValue(pass_word)
+        
+        let alert = UIAlertController(title: "Account created!", message: "Please login to your account.", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        self.present(alert, animated: true)
+    }
+    
+    @IBAction func signin(_ sender: UIButton) {
+//        let user_name = username.text!
+//        let pass_word = password.text!
+//
+//        self.ref?.child("UserAccountInfo").child(user_name)
     }
 
 

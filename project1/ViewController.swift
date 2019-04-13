442bc51dbb038166465298b224c54f2101144920
Xiwei Ma <xiwei@ucla.edu>
2019-04-12 21:20:04 -0700

    xcode

---
+++ project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project1
+//
+//  Created by Rosalie Ma on 4/12/19.
+//  Copyright © 2019 Rosalie Ma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

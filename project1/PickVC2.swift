1681cdc0a098ef99741641b3f571cbc568f557eb
Xiwei Ma <xiwei@ucla.edu>
2019-04-13 10:51:23 -0700

    pass picked img

--- project1/PickVC2.swift
+++ project1/PickVC2.swift
@@ -15,18 +15,20 @@ import CoreLocation
 class PickVC2: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var PickPic: UIImageView!
     let locationManager = CLLocationManager()
-    var locValue:CLLocationCoordinate2D
+    //var locValue
+    var loc_latitude = Double("")
+    var loc_longitude = Double("")
     
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-        self.locValue.latitude = Double("")!
-        self.locValue.longitude = Double("")!
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
+//    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+//        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+//        self.locValue.latitude = Double("")!
+//        self.locValue.longitude = Double("")!
+//    }
+//
+//    required init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -45,16 +47,23 @@ class PickVC2: UIViewController, CLLocationManagerDelegate {
         let pick_image:UIImage = load_img(position: locValue)
         self.PickPic.image = pick_image
         
+        //Only for test:
+        //self.PickPic.image = #imageLiteral(resourceName: "test")
+        
     }
     
     func load_img(position: CLLocationCoordinate2D) -> UIImage {
         //TODO: query server using location and decode the image
         //self.locValue.latitude, self.locValue.longitude
-        return 
+        
+        //TODO: If no image to pick
+        return
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        self.locValue = manager.location!.coordinate
+        let locValue = manager.location!.coordinate
+        self.loc_latitude = locValue.latitude
+        self.loc_longitude = locValue.longitude
         
         manager.stopUpdatingLocation()
     }
@@ -62,8 +71,14 @@ class PickVC2: UIViewController, CLLocationManagerDelegate {
     
     // Actions
     @IBAction func Confirm(_ sender: UIButton) {
-        
-        
+    }
+    
+    //send image to next view controller
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "sendPickImg" {
+            let confirmImg = segue.destination as! PickVC3
+            confirmImg.newImg = self.PickPic.image!
+        }
     }
     
 }

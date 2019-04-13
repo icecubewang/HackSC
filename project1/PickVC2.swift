7dc2e3e3deccab2f401531042072cd4d3e8877c8
John Yuan <yjohnyuan@gmail.com>
2019-04-13 15:48:03 -0700

    Distance Calculation with Picking random photo
    Distance Calculation with Picking random photo

--- project1/PickVC2.swift
+++ project1/PickVC2.swift
@@ -58,29 +58,85 @@ class PickVC2: UIViewController, CLLocationManagerDelegate {
         let lat = "\(locValue.latitude)".replacingOccurrences(of: ".", with: "_")
         let long = "\(locValue.longitude)".replacingOccurrences(of: ".", with: "_")
         let str_loc = lat + long
-        
-        
-
+        var  clost_loc = "nil"
         //TODO: If no image to pick
-        
-        ref?.child("Location").child("34_07261627446433-118_45177029832546").observeSingleEvent(of: .value, with: { (snapshot) in
+        // Look through the locations on Firebase to find the closest
+        ref?.child("Location").observeSingleEvent(of: .value, with: { (locsnapshot) in
             // Get user value
-            let imagedict = snapshot.value as? NSDictionary
-            
-            //print("val pic 1")
-            //print(imagedict!["Picture1"]!)
-            let index  = Int.random(in: 0 ..< imagedict!.allKeys.count)
-            let randout = Array(imagedict!)[index].value
-            print(randout)
-            
-            // Random image url generated from Firebase given a longitude, latitude.
+            //print(locsnapshot)
+            let locdict = locsnapshot.value as! NSDictionary
+            //print(locdict as Any)
+            //print("keys")
 
+            let  lockeys=locdict.allKeys
+            var  distance = CLLocationDistance(-1)
             
-            
+            for location in lockeys{
+                //print(location)
+                
+                var locconv = location as! String
+                var fullsplit = locconv.characters.split{$0 == "-"}.map(String.init)
+                let lat_tt = Double(fullsplit[0].replacingOccurrences(of: "_", with: "."))
+                let long_tt = Double(fullsplit[1].replacingOccurrences(of: "_", with: "."))
+                //print(lat_tt as Any)
+                //print(long_tt as Any)
+                
+                
+
+                let coordinate₀ = CLLocation(latitude: self.locValue.latitude, longitude: self.locValue.longitude)
+                let coordinate₁ = CLLocation(latitude: lat_tt!, longitude: long_tt!)
+                
+                let distanceInMeters = coordinate₀.distance(from: coordinate₁)
+                //print("distanceInMeters")
+                //print(distanceInMeters)// result is in meters
+                //print("distance")
+               // print(distance)
+                if distance == -1{
+                    distance=distanceInMeters
+                    clost_loc=location as! String
+                    //print ("clost_loc initial")
+                    //print (clost_loc)
+                }
+                else if Double(distanceInMeters)<distance {
+                    distance=distanceInMeters
+                    clost_loc=location as! String
+                    //print ("clost_loc FInal")
+                    //print (clost_loc)
+                }
+//                newm=
+//                if 0 < distance {
+//                    distance = new_min
+//                    clost_loc = location}
+            }
+            //print(locdict.allKeys)
+            // Random image url generated from Firebase given a longitude, latitude.
             // ...
+        
+            self.ref?.child("Location").child(clost_loc).observeSingleEvent(of: .value, with: { (snapshot) in
+                // Get user value
+                let imagedict = snapshot.value as? NSDictionary
+                
+                //print("val pic 1")
+                //print(imagedict!["Picture1"]!)
+                let index  = Int.random(in: 0 ..< imagedict!.allKeys.count)
+                let randout = Array(imagedict!)[index].value
+                print(randout)
+                
+                // Random image url generated from Firebase given a longitude, latitude.
+                // ...
+            }) { (error) in
+                print(error.localizedDescription)
+            }
+
+        
+        
         }) { (error) in
             print(error.localizedDescription)
         }
+
+        //print ("clost_loc")
+        //print (clost_loc)
+        // Given a location clost_loc, find a random image on Firebase.
 
         //return data()
     }
